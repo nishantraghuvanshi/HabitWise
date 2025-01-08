8ba93bec6b479bdd606e0638fcef77369ae163ed
Nishant Raghuvanshi <nishantraghuvanshi501@gmail.com>
2025-01-08 23:50:28 +0530

    Initial Setup

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -11,16 +11,18 @@ import { getDatabase } from "firebase/database";
 // import {...} from "firebase/functions";
 // import {...} from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA7F-Q39TNiqBYIoTW382wEJ4p2PTgEnTU",
-  authDomain: "habitwise-d09bb.firebaseapp.com",
-  projectId: "habitwise-d09bb",
-  storageBucket: "habitwise-d09bb.firebasestorage.app",
-  messagingSenderId: "664605725919",
-  appId: "1:664605725919:web:fca6ec97710da81546856a",
-  measurementId: "G-4GR6HRZE2T"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
